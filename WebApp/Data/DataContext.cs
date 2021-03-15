5fbf13ea6bd26f8315202519bea35791bd2f65de
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-03-15 20:37:59 -0300

    seeding db on creation

--- WebApp/Data/DataContext.cs
+++ WebApp/Data/DataContext.cs
@@ -11,5 +11,13 @@ namespace WebApp.Data
         }
         
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, Title = "Cheese burger", Description = "burger, cheese, pickles, bacon", Price = 18 },
+                new Product { Id = 2, Title = "Cheese burger", Description = "burger, cheese, pickles, bacon", Price = 18 },
+                new Product { Id = 3, Title = "Cheese burger", Description = "burger, cheese, pickles, bacon", Price = 18 }
+            );
+        }
     }
 }
