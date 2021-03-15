5d5df1705be069db0950c6e810636bf6238c1480
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-03-15 00:58:14 -0300

    scrolling to menu with link to id

--- WebApp/ClientApp/src/pages/Home/index.jsx
+++ WebApp/ClientApp/src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { IoAdd, IoRemove, IoBagAddOutline } from "react-icons/io5";
 
 import UnderlinedTitle from "../../components/UnderlinedTitle";
+import ScrollIntoView from "../../components/ScrollIntoView";
 
 import {
   HeroContainer,
@@ -40,47 +41,49 @@ function Home() {
         </BackgroundOverlay>
       </HeroContainer>
 
-      <MenuContainer>
-        <UnderlinedTitle>Cardápio</UnderlinedTitle>
+      <ScrollIntoView scrollToId="menu">
+        <MenuContainer id="menu">
+          <UnderlinedTitle>Cardápio</UnderlinedTitle>
 
-        <MenuCardGrid>
-          {[1, 2, 3, 4, 5].map((_, index) => (
-            <MenuCard key={String(index)}>
-              <img src="" alt="" />
-              <strong>Cheddar Bacon</strong>
-              <span>dois hamburgueres, cheddar, alface</span>
+          <MenuCardGrid>
+            {[1, 2, 3, 4, 5].map((_, index) => (
+              <MenuCard key={String(index)}>
+                <img src="" alt="" />
+                <strong>Cheddar Bacon</strong>
+                <span>dois hamburgueres, cheddar, alface</span>
 
-              <MenuCardOptionList>
-                <MenuCardOption width="38%">
-                  <MenuCardOptionBackground>
-                    <MenuCardOptionSelectAmountButton>
-                      <IoRemove color="#999" size={26} />
-                    </MenuCardOptionSelectAmountButton>
+                <MenuCardOptionList>
+                  <MenuCardOption width="38%">
+                    <MenuCardOptionBackground>
+                      <MenuCardOptionSelectAmountButton>
+                        <IoRemove color="#999" size={26} />
+                      </MenuCardOptionSelectAmountButton>
 
-                    <MenuCardAmount>1</MenuCardAmount>
+                      <MenuCardAmount>1</MenuCardAmount>
 
-                    <MenuCardOptionSelectAmountButton>
-                      <IoAdd color="#999" size={26} />
-                    </MenuCardOptionSelectAmountButton>
-                  </MenuCardOptionBackground>
-                </MenuCardOption>
+                      <MenuCardOptionSelectAmountButton>
+                        <IoAdd color="#999" size={26} />
+                      </MenuCardOptionSelectAmountButton>
+                    </MenuCardOptionBackground>
+                  </MenuCardOption>
 
-                <MenuCardOption width="38%">
-                  <MenuCardOptionBackground>
-                    <MenuCardPrice>R$ 16,90</MenuCardPrice>
-                  </MenuCardOptionBackground>
-                </MenuCardOption>
+                  <MenuCardOption width="38%">
+                    <MenuCardOptionBackground>
+                      <MenuCardPrice>R$ 16,90</MenuCardPrice>
+                    </MenuCardOptionBackground>
+                  </MenuCardOption>
 
-                <MenuCardOption>
-                  <Bag>
-                    <IoBagAddOutline size={32} color="#ffffff" />
-                  </Bag>
-                </MenuCardOption>
-              </MenuCardOptionList>
-            </MenuCard>
-          ))}
-        </MenuCardGrid>
-      </MenuContainer>
+                  <MenuCardOption>
+                    <Bag>
+                      <IoBagAddOutline size={32} color="#ffffff" />
+                    </Bag>
+                  </MenuCardOption>
+                </MenuCardOptionList>
+              </MenuCard>
+            ))}
+          </MenuCardGrid>
+        </MenuContainer>
+      </ScrollIntoView>
     </>
   );
 }
