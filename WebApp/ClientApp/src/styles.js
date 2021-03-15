ecc530802a491835db15c553f148028317502934
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-03-15 00:25:44 -0300

    adding selectors for base styles

--- WebApp/ClientApp/src/styles.js
+++ WebApp/ClientApp/src/styles.js
@@ -1,10 +1,15 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: 'Montserrat', sans-serif;
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-family: 'Montserrat', sans-serif;
+  }
 
   :root {
     --light: #eeeeee;
