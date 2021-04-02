aa8b2d4e123317858a7801b0955a521eb55ae020
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-04-02 00:08:42 -0300

    adding react-toastify

--- WebApp/ClientApp/src/App.jsx
+++ WebApp/ClientApp/src/App.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 import { BagProvider } from "./contexts/BagContext";
 import { UserProvider } from "./contexts/UserContext";
@@ -19,6 +21,16 @@ function App() {
           <Footer />
         </BagProvider>
       </UserProvider>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={2000}
+        pauseOnHover={false}
+        closeButton={false}
+        newestOnTop
+        closeOnClick
+        draggable
+        limit={3}
+      />
     </>
   );
 }
