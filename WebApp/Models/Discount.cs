2e7ebdfa84abc1f384956950ffbd69a55083f73b
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-04-19 16:26:47 -0300

    removing data annotations

--- WebApp/Models/Discount.cs
+++ WebApp/Models/Discount.cs
@@ -8,13 +8,11 @@ namespace WebApp.Models
         public int Id { get; set; }
         
         [Required(ErrorMessage = "Este campo é obrigatório")]
-        [Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior que 0")]
         public int Amount { get; set;  }
         
         [Required(ErrorMessage = "Este campo é obrigatório")]
         public int ProductId { get; set; }
         
-        [Required(ErrorMessage = "Este campo é obrigatório")]
         public Product Product { get; set; }
     }
 }
