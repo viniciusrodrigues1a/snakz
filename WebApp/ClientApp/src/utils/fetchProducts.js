c15b5bc1d32dfd874f64c0cc92860e6fd8e55803
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-04-28 15:11:01 -0300

    format price with discount

--- WebApp/ClientApp/src/utils/fetchProducts.js
+++ WebApp/ClientApp/src/utils/fetchProducts.js
@@ -4,9 +4,12 @@ export async function fetchProducts() {
   const response = await fetch("/products").then(res => res.json());
   const formattedResponse = response.map(p => {
     const amount = 1;
+    const formattedDiscountPrice = getFormattedDiscountPrice(p);
     return {
       ...p.product,
+      discountId: p.discount?.id,
       amount,
+      formattedDiscountPrice,
       formattedPrice: formatPrice(p.product.price),
       formattedSubtotal: formatPrice(p.product.price * amount),
     };
@@ -21,4 +24,12 @@ export async function fetchProduct(id) {
     formattedPrice: formatPrice(response.price),
   };
   return formattedResponse;
+}
+
+function getFormattedDiscountPrice(p) {
+  if (p.discount) {
+    return formatPrice(p.product.price - p.discount.amount);
+  }
+
+  return null;
 }
