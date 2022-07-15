9933ebea7534e69365515475ee9a75ad1406bb9e
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2022-07-14 22:13:14 -0300

    Making modal position fixed

--- WebApp/ClientApp/src/pages/ProductsManagement/Modal/styles.js
+++ WebApp/ClientApp/src/pages/ProductsManagement/Modal/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  position: absolute;
+  position: fixed;
   z-index: 999;
   display: flex;
   justify-content: center;
